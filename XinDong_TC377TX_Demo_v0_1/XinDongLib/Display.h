3d5910077cd7b3ba1179e6c26c903cd77023708a
yikai04 <128439989+yikai04@users.noreply.github.com>
2025-07-02 12:02:10 +0800

    2_7_2025

--- XinDong_TC377TX_Demo_v0_1/XinDongLib/Display.h
+++ XinDong_TC377TX_Demo_v0_1/XinDongLib/Display.h
@@ -1,8 +1,8 @@
 /**********************************************************************************************************************
  * Author: Yikai Huang
- * Description:
+ * Description: 21/5/2025
  * 
- * Version:
+ * Version:1.0
  *********************************************************************************************************************/
 
 #ifndef XINDONGLIB_DISPLAY_H_
@@ -11,15 +11,27 @@
 /*********************************************************************************************************************/
 /*-----------------------------------------------------Includes------------------------------------------------------*/
 /*********************************************************************************************************************/
-#include "XinDong_Config.h"
+#include "OLED_Data.h"
+#include "EI2C.h"
 
 /*********************************************************************************************************************/
 /*------------------------------------------------------Macros-------------------------------------------------------*/
 /*********************************************************************************************************************/
+/*FontSize参数取值*/
+/*此参数值不仅用于判断，而且用于计算横向字符偏移，默认值为字体像素宽度*/
+#define OLED_8X16				8
+#define OLED_6X8				6
+
+/*IsFilled参数数值*/
+#define OLED_UNFILLED			0
+#define OLED_FILLED				1
+
+#define OLED_I2C_ADDRESS        0x78	//I2C地址
 
 /*********************************************************************************************************************/
 /*-------------------------------------------------Global variables--------------------------------------------------*/
 /*********************************************************************************************************************/
+extern uint8 OLED_DisplayBuf[8][128];
 
 /*********************************************************************************************************************/
 /*-------------------------------------------------Data Structures---------------------------------------------------*/
@@ -28,10 +40,50 @@
 /*********************************************************************************************************************/
 /*--------------------------------------------Private Variables/Constants--------------------------------------------*/
 /*********************************************************************************************************************/
-
+EI2C_Typedef OLED_I2C;	//I2C结构体
 /*********************************************************************************************************************/
 /*------------------------------------------------Function Prototypes------------------------------------------------*/
 /*********************************************************************************************************************/
+/*初始化函数*/
+void OLED_Init(void);
 
+/*更新函数*/
+void OLED_Update(void);
+void OLED_UpdateArea(uint8 X, uint8 Y, uint8 Width, uint8 Height);
+
+/*显存控制函数*/
+void OLED_Clear(void);
+void OLED_ClearArea(uint8 X, uint8 Y, uint8 Width, uint8 Height);
+void OLED_Reverse(void);
+void OLED_ReverseArea(uint8 X, uint8 Y, uint8 Width, uint8 Height);
+
+/*显示函数*/
+void OLED_ShowChar(uint8 X, uint8 Y, char Char, uint8 FontSize);
+void OLED_ShowString(uint8 X, uint8 Y, char *String, uint8 FontSize);
+void OLED_ShowNum(uint8 X, uint8 Y, uint32 Number, uint8 Length, uint8 FontSize);
+void OLED_ShowSignedNum(uint8 X, uint8 Y, uint32 Number, uint8 Length, uint8 FontSize);
+void OLED_ShowHexNum(uint8 X, uint8 Y, uint32 Number, uint8 Length, uint8 FontSize);
+void OLED_ShowBinNum(uint8 X, uint8 Y, uint32 Number, uint8 Length, uint8 FontSize);
+void OLED_ShowFloatNum(uint8 X, uint8 Y, double Number, uint8 IntLength, uint8 FraLength, uint8 FontSize);
+void OLED_ShowChinese(uint8 X, uint8 Y, char *Chinese);
+void OLED_ShowImage(uint8 X, uint8 Y, uint8 Width, uint8 Height, const uint8 *Image);
+void OLED_Printf(uint8 X, uint8 Y, uint8 FontSize, char *format, ...);
+
+/*绘图函数*/
+void OLED_DrawPoint(uint8 X, uint8 Y);
+uint8 OLED_GetPoint(uint8 X, uint8 Y);
+void OLED_DrawLine(uint8 X0, uint8 Y0, uint8 X1, uint8 Y1);
+void OLED_DrawRectangle(uint8 X, uint8 Y, uint8 Width, uint8 Height, uint8 IsFilled);
+void OLED_DrawTriangle(uint8 X0, uint8 Y0, uint8 X1, uint8 Y1, uint8 X2, uint8 Y2, uint8 IsFilled);
+void OLED_DrawCircle(uint8 X, uint8 Y, uint8 Radius, uint8 IsFilled);
+void OLED_DrawEllipse(uint8 X, uint8 Y, uint8 A, uint8 B, uint8 IsFilled);
+void OLED_DrawArc(uint8 X, uint8 Y, uint8 Radius, uint16 StartAngle, uint16 EndAngle, uint8 IsFilled);
+
+//自定义函数
+/*********************函数声明*/
+// void OLED_Rotation_C_P(int8 CX, int8 CY, float* PX, float* PY, int16 Angle);	//点旋转
+// void OLED_Rotation_Block(int8 X, int8 Y, int8 Width, int16 Angle);			//区块旋转
+// void bresenham_ellipse(int x0, int y0, int rx, int ry);
+// void ellipse_algorithm(int8 x0, int8 y0, int8 a, int8 b);
 
 #endif /* XINDONGLIB_DISPLAY_H_ */
